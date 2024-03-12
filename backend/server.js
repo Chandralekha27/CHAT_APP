77cc13fcbf91313fa2484438bfde90716d533768
chandralekha <chandralekha01227@gmail.com>
2024-03-12 18:17:47 +0530

    frontend and models

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,21 @@ const express = require('express');
 // import the dummy data as chats
 const { chats } = require('./data/data');
 
+//import websocket
+const ws = require('ws');
+
+const wss = new ws.Server({port: 7071});
+
+wss.on('connection',()=>{
+    console.log("Connected to socket")
+})
+
 //create the web server
 const app = express();
 
 //first api to just see something on the browser
 app.get("/", (req, res) =>{
-    res.send("Hi there!");
+    res.send("Hi there!!");
 });
 
 // basic api to test the chat application
