e5ca993c7cfd0a47c7abc457c2b8a13d21493ce9
chandralekha <chandralekha01227@gmail.com>
2024-03-13 22:50:54 +0530

    register and login endpoints for user

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const {getUserByName, getUser, sayHello} = require('../controller/user')
+const {getUserByName, getUser, registerUser, sayHello, loginUser} = require('../controller/user')
 
 router.param("username",getUserByName);
 router.get("/getUserByName/:username",getUser);
 router.get("/hello",sayHello);
+router.post("/registerUser",registerUser);
+router.post("/login",loginUser);
 
 module.exports = router;
